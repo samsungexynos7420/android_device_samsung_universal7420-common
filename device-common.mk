c895c7a007719324055484810e7cdc57b529ddd2
Stricted <info@stricted.net>
2018-10-01 11:03:41 +0200

    universal7420: build keymaster hidl and packages
    Change-Id: I72a58d7c5e72fb429cc18850d44665b8a34a8a15

--- device-common.mk
+++ device-common.mk
@@ -117,6 +117,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.samsung
     
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0 \
+    android.hardware.keymaster@3.0-service \
+    android.hardware.keymaster@3.0-impl \
+    libkeymaster3device
+
 # Media profile
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_codecs.xml \
