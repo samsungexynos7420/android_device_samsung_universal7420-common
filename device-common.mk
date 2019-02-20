91893bd08c09cae0eb2b463b1cee2a9076aee4ec
Sean Callanan <spyffe@google.com>
2019-02-20 12:45:08 -0800

    universal7420: Switch to IMapper 2.1
    Test: display CTS
    Bug: 62535446
    Change-Id: I2d6d0d118014eb2b99a6832b4c900ae21c062517

--- device-common.mk
+++ device-common.mk
@@ -101,7 +101,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
-    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.mapper@2.0-impl-2.1 \
     gralloc.exynos5 \
     hwcomposer.exynos5 \
     libfimg \
