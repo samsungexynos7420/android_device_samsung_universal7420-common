c296df5c3be1d0d1643e8152905ac18c8c268579
enesuzun2002 <enesmuratuzun@outlook.com.tr>
2021-02-17 14:45:00 +0300

    universal7420: add media configuration files
    Change-Id: I3ce8540c5b1132d19f34880e5a10fa734a3dfdd3

--- device-common.mk
+++ device-common.mk
@@ -106,6 +106,15 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
     android.hardware.health@2.0-service
     
+# Media profile
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_codecs.xml \
+	$(LOCAL_PATH)/configs/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_codecs_performance.xml \
+	$(LOCAL_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_codecs_google_audio.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_codecs_google_telephony.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_codecs_google_video.xml
+
 # Memory
 PRODUCT_PACKAGES += \
     memtrack.exynos5 \
