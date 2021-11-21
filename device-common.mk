a613f37435f7a9012c3e2af986b55137305d611f
Tim Zimmermann <tim@linux4.de>
2021-11-21 08:57:14 +0100

    universal7420: switch to new AIDL light HAL
    Change-Id: I7917b1da162e28187216fc8f55f164fae2ec8eef
    Signed-off-by: Francescodario Cuzzocrea <bosconovic@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -204,7 +204,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-	android.hardware.light@2.0-service.samsung
+	android.hardware.light-service.samsung
 
 # Media profile
 PRODUCT_COPY_FILES += \
