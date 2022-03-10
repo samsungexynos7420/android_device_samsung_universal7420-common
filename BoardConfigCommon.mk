2dcfa5c8d059ec00bb337229f36cd29f36724ef5
ivanmeler <i_ivan@windowslive.com>
2022-03-10 10:44:39 +0000

    We dont really want lineage recovery do we

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -99,6 +99,7 @@ BOARD_KERNEL_SEPARATED_DT := true
 BOARD_RAMDISK_USE_XZ := true
 TARGET_KERNEL_CLANG_COMPILE := false
 TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
+BOARD_USES_FULL_RECOVERY_IMAGE := false
 
 # LED
 RED_LED_PATH := "/sys/class/leds/led_r/brightness"
