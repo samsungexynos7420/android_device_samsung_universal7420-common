1958821ea75ea33c1f5fd7dd31aff8e2bfced4c6
kde <55951275+fakemanoan@users.noreply.github.com>
2022-06-07 15:03:19 +0100

    Add shim for GPSD
    Fixes GPS

--- device-common.mk
+++ device-common.mk
@@ -160,7 +160,8 @@ PRODUCT_PACKAGES += \
 # GNSS
 PRODUCT_PACKAGES += \
 	android.hardware.gnss@1.0-impl.universal7420 \
-	android.hardware.gnss@1.0-service.universal7420
+	android.hardware.gnss@1.0-service.universal7420 \
+	gpsd_shim
 
 # Graphics
 PRODUCT_PACKAGES += \
