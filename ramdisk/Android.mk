7b13f2bcbfd36766649a1e59f5a136012d1520bd
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2022-08-22 21:50:52 +0100

    LOCAL_MODULE_STEM := ueventd.rc
    Test: Boots

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -29,6 +29,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= ueventd.samsungexynos7420.rc
+LOCAL_MODULE_STEM  := ueventd.rc
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= ueventd.samsungexynos7420.rc
 LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_ETC)
