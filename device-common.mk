fc889e761690ad3d73af6f842c97fd55376fc00f
ivanmeler <i_ivan@windowslive.com>
2022-08-31 17:48:58 +0000

    universal7420: Add custom audio service
    with reverts of:
    LineageOS/android_hardware_interfaces@3f8f599
    LineageOS/android_hardware_interfaces@d90c474

--- device-common.mk
+++ device-common.mk
@@ -47,7 +47,7 @@ PRODUCT_PACKAGES += \
 	audio.usb.default \
 	tinymix \
 	libtinycompress \
-	android.hardware.audio.service \
+	android.hardware.audio.service.universal7420 \
 	android.hardware.audio@7.0-impl:32 \
 	android.hardware.audio.effect@7.0-impl:32 \
 	android.hardware.bluetooth.audio@2.0-impl:32 \
