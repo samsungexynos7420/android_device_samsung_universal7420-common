3c788273229dbd38a6235e8ef85e97b3a4edd5c2
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2022-10-02 15:38:46 +0100

    No more OSS mobicore
    For now

--- device-common.mk
+++ device-common.mk
@@ -177,12 +177,6 @@ PRODUCT_COPY_FILES += \
 	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
-# Mobicore
-PRODUCT_PACKAGES += \
-    mcDriverDaemon \
-    libMcClient \
-    libMcRegistry
-
 # NFC
 PRODUCT_PACKAGES += \
 	libnfc-nci \
