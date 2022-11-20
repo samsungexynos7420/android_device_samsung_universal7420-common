81bb0f763dd418ae4b954cbdee5d9dc79a0c5e94
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2022-11-20 21:22:58 +0000

    swap to le video and build 32bit bluetooth audio
    Change-Id: I3209f98fcc353a7536c9d1e1fd0b399de9fe8d50

--- device-common.mk
+++ device-common.mk
@@ -88,7 +88,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
 	android.hardware.bluetooth@1.0-impl \
 	android.hardware.bluetooth@1.0-service \
-	android.hardware.bluetooth.audio@2.0-impl \
+	android.hardware.bluetooth.audio@2.0-impl:32 \
 	audio.bluetooth.default \
 	libbt-vendor
 
@@ -187,7 +187,7 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
-	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
+	frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
 # NFC
 PRODUCT_PACKAGES += \
