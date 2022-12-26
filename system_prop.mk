e14d1851cb5a390ff5777bfaed9a89abed4e7ad3
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2022-12-26 15:03:20 +0000

    universal7420: update codec ranking

--- system_prop.mk
+++ system_prop.mk
@@ -55,7 +55,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # OMX
 #
 PRODUCT_PROPERTY_OVERRIDES += \
-	debug.stagefright.ccodec=0
+	debug.stagefright.ccodec=0 \
+	debug.stagefright.omx_default_rank=0 \
+	debug.stagefright.omx_default_rank.sw-audio=1
 
 #
 # Miracast (Wireless Display)
