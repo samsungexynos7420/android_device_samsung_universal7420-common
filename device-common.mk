95e65b6a4114f6306a431da3f042b48b906f3dc1
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2022-12-26 14:57:18 +0000

    universal7420: update some ramdisk stuff
    * fix some adb stuff
    * fix cpusets not being correctly applied on top-app
    * remove unneeded files
    * reserve 128MB for data partition for system
    * fix TSP perms
    * change charger permissions
    * BFQ i/o scheduler

--- device-common.mk
+++ device-common.mk
@@ -85,8 +85,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
 	android.hardware.bluetooth@1.0-impl \
 	android.hardware.bluetooth@1.0-service \
-	android.hardware.bluetooth.audio@2.0-impl \
-	audio.bluetooth.default \
 	libbt-vendor
 
 # Camera
--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -21,13 +21,6 @@ LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE            := init.zerofltexx.rc
-LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := init.zerofltexx.rc
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE		:= ueventd.samsungexynos7420.rc
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= ueventd.samsungexynos7420.rc
