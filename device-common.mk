9848b2036ef9fc1ea328e840310825d49744a176
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2023-01-02 16:55:10 +0000

    universal7420: rename GNSS to universal7420

--- device-common.mk
+++ device-common.mk
@@ -160,7 +160,7 @@ PRODUCT_PACKAGES += \
 # GNSS
 PRODUCT_PACKAGES += \
 	android.hardware.gnss@1.0-impl.universal7420 \
-	android.hardware.gnss@1.0-service
+	android.hardware.gnss@1.0-service.universal7420
 
 # Graphics
 PRODUCT_PACKAGES += \
