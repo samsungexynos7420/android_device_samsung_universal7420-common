cd0ff91b2f6c5edbb6fdf9cb297bc5d10f518fb8
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2023-01-14 14:37:51 +0000

    universal7420: swap to samsung vibrator service

--- device-common.mk
+++ device-common.mk
@@ -309,8 +309,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-	android.hardware.vibrator@1.0-impl \
-	android.hardware.vibrator@1.0-service
+	android.hardware.vibrator@1.3-service.samsung
 
 # Wifi
 PRODUCT_PACKAGES += \
