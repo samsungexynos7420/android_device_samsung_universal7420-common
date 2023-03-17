f87620aa9584fffab82da104b7ca6b489c094abb
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2023-03-17 14:44:59 +0000

    universal7420: remove GPSD shim
    doesnt work.
    Use libsensor patch in 7420_patches instead

--- shims/gpsd/gps_symbols.cpp
+++
@@ -1,5 +0,0 @@
-#include <string.h>
-#include <openssl/ssl.h>
-
-extern "C" void _ZN7android13SensorManager16createEventQueueENS_7String8Ei();
-extern "C" void _ZN7android13SensorManager16createEventQueueENS_7String8Ei(){}
