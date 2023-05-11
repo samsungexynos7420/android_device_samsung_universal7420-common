955ec6d74dd6c9f61240276263f6b4d5b56e7f65
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2023-05-11 15:16:45 +0100

    universal7420: Build samsung thermal HAL
    * Temporary values. Pretty sure shutoff for the CPU is 115C but using 95C to be safe
    * Not sure what the dangerous battery value is, so set to the same as S7.
    * Look into adding other values like GPU/ISP like the S7, but this will require kernel work

--- device-common.mk
+++ device-common.mk
@@ -312,6 +312,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	vendor.lineage.touch@1.0-service.samsung
 
+# Thermal
+PRODUCT_PACKAGES += \
+	android.hardware.thermal@2.0-service.samsung
+
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+
 # Trust
 PRODUCT_PACKAGES += \
 	vendor.lineage.trust@1.0-service
