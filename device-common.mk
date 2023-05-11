47b55f4b9d736cdb761ed7d95f1ab3b75703df80
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2023-05-11 16:04:20 +0100

    universal7420: Swap to 2.1 ver of mapper

--- device-common.mk
+++ device-common.mk
@@ -179,8 +179,8 @@ PRODUCT_PACKAGES += \
 	android.hardware.graphics.allocator@2.0-impl \
 	android.hardware.graphics.allocator@2.0-service \
 	android.hardware.graphics.composer@2.1-service \
-	android.hardware.graphics.mapper@2.0-impl
-
+	android.hardware.graphics.mapper@2.0-impl-2.1
+	
 # Health
 PRODUCT_PACKAGES += \
 	android.hardware.health@2.0-impl \
