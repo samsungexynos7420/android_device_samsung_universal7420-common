c3aacb37a6adcb4aa8c62acc73d0357e5e30403c
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2023-05-13 12:10:50 +0100

    universal7420: update HWC props to coincide with new BSP
    Modification by ivan from 8890q sources. Easier to change here.

--- system_prop.mk
+++ system_prop.mk
@@ -70,8 +70,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # HWC
 PRODUCT_PROPERTY_OVERRIDES += \
+	ro.vendor.winupdate=1 \
 	debug.sf.latch_unsignaled=1 \
-	debug.hwc.winupdate=1
 	debug.sf.disable_backpressure=1 \
 	ro.surface_flinger.max_frame_buffer_acquired_buffers=3
 
