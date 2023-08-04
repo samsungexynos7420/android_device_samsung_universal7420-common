629843128dd9ef758425d6fbf956ce506fb26819
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2023-08-04 12:13:55 +0100

    Update dalvik props

--- system_prop.mk
+++ system_prop.mk
@@ -25,13 +25,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Dalvik
 #
 PRODUCT_PROPERTY_OVERRIDES += \
-	dalvik.vm.image-dex2oat-filter=speed \
-	dalvik.vm.dex2oat-filter=speed \
-	dalvik.vm.heapgrowthlimit=256m \
 	dalvik.vm.heapstartsize=8m \
+	dalvik.vm.heapgrowthlimit=192m \
 	dalvik.vm.heapsize=512m \
 	dalvik.vm.heaptargetutilization=0.75 \
-	dalvik.vm.heapminfree=512k \
+	dalvik.vm.heapminfree=2m \
 	dalvik.vm.heapmaxfree=8m \
 	dalvik.vm.dex2oat64.enabled=true
 
