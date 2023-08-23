1fecc0c77bd356923799264aa1e107d1a84a6022
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2023-08-23 14:01:34 +0100

    universal7420: remove HWUI performance prop
    Unnecessary

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -115,9 +115,6 @@ BOARD_HDMI_INCAPABLE := true
 # HWCServices - requires framework support
 BOARD_USES_HWC_SERVICES := true
 
-# HWUI
-HWUI_COMPILE_FOR_PERF := true
-
 #  ION
 TARGET_USES_ION := true
 
