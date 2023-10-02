4825654f0f4830acad68d8a72fbd23904ff44f20
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2023-10-02 14:05:17 +0100

    universal7420: use libutils-v32 for some files
    GPSD, LHD seem to crash with newer libutils in some scenarios

--- extract-files.sh
+++ extract-files.sh
@@ -78,4 +78,12 @@ BLOB_ROOT="$ANDROID_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary
 # replace SSLv3_client_method with SSLv23_method
 sed -i "s/SSLv3_client_method/SSLv23_method\x00\x00\x00\x00\x00\x00/" $BLOB_ROOT/bin/gpsd
 
+# Replace libutils with libutils-v32
+"${PATCHELF}" --replace-needed libutils.so libutils-v32.so $BLOB_ROOT/vendor/bin/hw/gpsd
+"${PATCHELF}" --replace-needed libutils.so libutils-v32.so $BLOB_ROOT/vendor/bin/hw/lhd
+"${PATCHELF}" --replace-needed libutils.so libutils-v32.so $BLOB_ROOT/vendor/lib/libexynoscamera.so
+"${PATCHELF}" --replace-needed libutils.so libutils-v32.so $BLOB_ROOT/vendor/lib/libexynoscamera3.so
+"${PATCHELF}" --replace-needed libutils.so libutils-v32.so $BLOB_ROOT/vendor/lib64/libexynoscamera.so
+"${PATCHELF}" --replace-needed libutils.so libutils-v32.so $BLOB_ROOT/vendor/lib64/libexynoscamera3.so
+
 "${MY_DIR}/setup-makefiles.sh"
