be41bd00616e5ab2c457f4b6db3352e8b78511ea
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2023-10-02 12:34:24 +0100

    universal7420: use AIDL clearkey

--- device-common.mk
+++ device-common.mk
@@ -132,7 +132,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
 	android.hardware.drm@1.0-impl \
 	android.hardware.drm@1.0-service \
-	android.hardware.drm@1.4-service.clearkey
+	android.hardware.drm-service.clearkey
 
 # DTB
 PRODUCT_HOST_PACKAGES += \
