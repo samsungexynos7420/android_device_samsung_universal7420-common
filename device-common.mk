d3f8f22cfc94e14d49ecfb7ee9cb2c85fd7f825c
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2023-10-02 15:09:10 +0100

    64 bit bluetooth

--- device-common.mk
+++ device-common.mk
@@ -66,7 +66,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-	android.hardware.bluetooth@1.0-impl \
+	android.hardware.bluetooth@1.0-impl:64 \
 	android.hardware.bluetooth@1.0-service \
 	libbt-vendor
 
