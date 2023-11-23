0c64d02a975682b3c8bd875d697ed0d01a78e5e2
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2023-11-23 19:32:18 +0000

    universal7420: build GNSS hal

--- device-common.mk
+++ device-common.mk
@@ -123,6 +123,11 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service \
     android.hardware.gatekeeper@1.0-impl
 
+# GNSS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0-impl.universal7420 \
+    android.hardware.gnss@1.0-service.universal7420
+
 # Graphics
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
