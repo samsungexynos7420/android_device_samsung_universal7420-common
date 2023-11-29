eba45ec95bfc9c7be4a8a3931883825bb786e2d1
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2023-11-29 11:20:42 +0000

    Update setup-makefiles.sh

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -24,23 +24,55 @@ export INITIAL_COPYRIGHT_YEAR=2015
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
+ANDROID_ROOT="${MY_DIR}/../../.."
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
-. "$HELPER"
+source "$HELPER"
 
 # Initialize the helper
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
+setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
-# Copyright headers and guards
-write_headers "zerofltecan zerofltexx zeroltecan zeroltexx"
+# Warning headers and guards
+write_headers "zerofltecan zeroltecan zerofltexx zeroltexx"
 
-# The standard blobs
-write_makefiles "$MY_DIR"/proprietary-files.txt true
+write_makefiles "${MY_DIR}/proprietary-files.txt" true
+
+###################################################################################################
+# CUSTOM PART START                                                                               #
+###################################################################################################
+
+OUTDIR=vendor/$VENDOR/$DEVICE
+
+(cat << EOF) >> $ANDROID_ROOT/$OUTDIR/Android.mk
+include \$(CLEAR_VARS)
+
+EGL_LIBS := libGLES_mali.so libOpenCL.so libOpenCL.so.1 libOpenCL.so.1.1 vulkan.exynos5.so
+
+EGL_32_SYMLINKS := \$(addprefix \$(TARGET_OUT_VENDOR)/lib/,\$(EGL_LIBS))
+\$(EGL_32_SYMLINKS): \$(LOCAL_INSTALLED_MODULE)
+	@echo "Symlink: EGL 32-bit lib: \$@"
+	@mkdir -p \$(dir \$@)
+	@rm -rf \$@
+	\$(hide) ln -sf /vendor/lib/egl/libGLES_mali.so \$@
+
+EGL_64_SYMLINKS := \$(addprefix \$(TARGET_OUT_VENDOR)/lib64/,\$(EGL_LIBS))
+\$(EGL_64_SYMLINKS): \$(LOCAL_INSTALLED_MODULE)
+	@echo "Symlink: EGL 64-bit lib : \$@"
+	@mkdir -p \$(dir \$@)
+	@rm -rf \$@
+	\$(hide) ln -sf /vendor/lib64/egl/libGLES_mali.so \$@
+
+ALL_DEFAULT_INSTALLED_MODULES += \$(EGL_32_SYMLINKS) \$(EGL_64_SYMLINKS)
+
+EOF
+
+###################################################################################################
+# CUSTOM PART END                                                                                 #
+###################################################################################################
 
 # Done
 write_footers
