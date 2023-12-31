afeb66bfcf5fc13b9217d7a66e993bffd9da02db
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2023-12-31 21:29:33 +0000

    Use 4gb dalvik config
    It is close to what we are using anyway

--- device-common.mk
+++ device-common.mk
@@ -414,5 +414,8 @@ PRODUCT_COPY_FILES += \
 	$(COMMON_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
 	$(COMMON_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
+
 # call the proprietary setup
 $(call inherit-product, vendor/samsung/universal7420-common/universal7420-common-vendor.mk)
