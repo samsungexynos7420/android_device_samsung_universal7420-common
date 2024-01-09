6d0626de17bd61e84d86a5345806939208b23426
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2024-01-09 15:17:58 +0000

    Support building recovery

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -14,6 +14,14 @@ LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE		:= init.recovery.samsungexynos7420.rc
+LOCAL_MODULE_TAGS	:= optional
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/init.recovery.samsungexynos7420.rc
+LOCAL_MODULE_PATH	:= $(TARGET_RECOVERY_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE            := init.baseband.rc
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := etc/init.baseband.rc
