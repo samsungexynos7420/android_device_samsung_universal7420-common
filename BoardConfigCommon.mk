0bbad595a1b38e59046b7e16cd806751d073df41
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2024-02-24 12:42:15 +0000

    Remove deprecated bluetooth flag
    this option does literally nothing, why is it here

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -52,7 +52,6 @@ BACKLIGHT_PATH := "/sys/class/backlight/panel/brightness"
 BOARD_CUSTOM_BT_CONFIG := $(COMMON_PATH)/configs/bluetooth/libbt_vndcfg.txt
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
-BOARD_HAVE_SAMSUNG_BLUETOOTH := true
 
 # Bootloader
 TARGET_NO_BOOTLOADER := true
