5787028091f1df678f390b1191a2c4e0ce93fa4d
Bruno Martins <bgcngm@gmail.com>
2024-03-21 16:31:24 +0000

    universal7420-common: Remove deprecated trust HAL
    Change-Id: Ic5ac79b4ca60b87a97622877b97f806c07cab907

--- device-common.mk
+++ device-common.mk
@@ -385,10 +385,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	android.hardware.thermal@2.0-service.samsung
 
-# Trust
-PRODUCT_PACKAGES += \
-	vendor.lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
 	android.hardware.usb@1.3-service.basic
