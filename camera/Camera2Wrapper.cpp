f2a3312f10b0a1f59fe778ce05f4878d9ba090ee
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2024-12-29 12:43:28 +0000

    Convert android string to c_str
    string() is not allowed anymore

--- camera/Camera2Wrapper.cpp
+++ camera/Camera2Wrapper.cpp
@@ -100,7 +100,7 @@ static char * camera2_fixup_getparams(int id __unused, const char * settings)
 #endif
 
     android::String8 strParams = params.flatten();
-    char *ret = strdup(strParams.string());
+    char *ret = strdup(strParams.c_str());
 
     return ret;
 }
@@ -124,7 +124,7 @@ static char * camera2_fixup_setparams(int id __unused, const char * settings)
 #endif
 
     android::String8 strParams = params.flatten();
-    char *ret = strdup(strParams.string());
+    char *ret = strdup(strParams.c_str());
 
     return ret;
 }
