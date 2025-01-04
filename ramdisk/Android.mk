8914dcd95c8bd72708fee4e4773be04847086e9c
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2025-01-04 12:12:34 +0000

    Fix up typo

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -12,10 +12,10 @@ LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.recovery.samsungexynos8890.rc
+LOCAL_MODULE		:= init.recovery.samsungexynos7420.rc
 LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES        := etc/init.recovery.samsungexynos8890.rc
+LOCAL_SRC_FILES        := etc/init.recovery.samsungexynos7420.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
