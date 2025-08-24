6ae04e5b88ba4290a55e3682c18c947002c6a57f
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2025-08-24 23:28:45 +0100

    remove mediaserver shim

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -172,7 +172,6 @@ TARGET_LD_SHIM_LIBS += \
 	/vendor/lib64/libexynoscamera.so|/vendor/lib64/libexynoscamera_shim.so \
 	/vendor/lib/libbauthserver.so|/vendor/lib/libbauthtzcommon_shim.so \
 	/vendor/lib64/libbauthserver.so|/vendor/lib64/libbauthtzcommon_shim.so \
-	/system/bin/mediaserver|/vendor/lib/libstagefright_shim.so \
 	/vendor/bin/hw/gpsd|sensor_shim.so \
 	/vendor/bin/hw/lhd|sensor_shim.so
 
